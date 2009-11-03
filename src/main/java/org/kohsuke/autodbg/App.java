7e5667f5296c64a6ba9e087bc403b5cef37d5af6
Kohsuke Kawaguchi <kk@kohsuke.org>
2009-11-02 18:25:39 -0800

    initial

---
+++ src/main/java/org/kohsuke/autodbg/App.java
@@ -0,0 +1,13 @@
+package org.kohsuke.autodbg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
