ed9b3890a77428c0c0a6d2489dfa546d30f72009
Kohsuke Kawaguchi <kk@kohsuke.org>
2009-11-05 19:43:59 -0800

    added one more connector

--- src/main/java/org/kohsuke/youdebug/VMFactory.java
+++ src/main/java/org/kohsuke/youdebug/VMFactory.java
@@ -5,9 +5,13 @@ import com.sun.jdi.VirtualMachineManager;
 import com.sun.jdi.connect.AttachingConnector;
 import com.sun.jdi.connect.Connector.Argument;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.List;
 
 /**
  * Connects to {@link VM} in various ways.
@@ -20,7 +24,7 @@ public class VMFactory {
      */
     public static VM connectRemote(String host, int port) throws IllegalConnectorArgumentsException, IOException {
         VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
-        AttachingConnector a = findConnector(vmm, "com.sun.jdi.SocketAttach");
+        AttachingConnector a = findConnector("com.sun.jdi.SocketAttach", vmm.attachingConnectors());
         Map<String,Argument> args = a.defaultArguments();
         args.get("hostname").setValue(host);
         args.get("port").setValue(String.valueOf(port));
@@ -29,16 +33,24 @@ public class VMFactory {
 
     public static VM connectLocal(int pid) throws IllegalConnectorArgumentsException, IOException {
         VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
-        AttachingConnector a = findConnector(vmm, "com.sun.jdi.ProcessAttach");
+        AttachingConnector a = findConnector("com.sun.jdi.ProcessAttach", vmm.attachingConnectors());
         Map<String,Argument> args = a.defaultArguments();
         args.get("pid").setValue(String.valueOf(pid));
         return new VM(a.attach(args));
     }
 
-    private static AttachingConnector findConnector(VirtualMachineManager vmm, String name) {
-        for (AttachingConnector ac : vmm.attachingConnectors())
-            if (ac.name().equals(name))
-                return ac;
+    public static VM launch(String commandLine) throws IllegalConnectorArgumentsException, IOException, VMStartException {
+        VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
+        LaunchingConnector a = findConnector("com.sun.jdi.CommandLineLaunch", vmm.launchingConnectors());
+        Map<String,Argument> args = a.defaultArguments();
+        args.get("main").setValue(commandLine);
+        return new VM(a.launch(args));
+    }
+
+    private static <T extends Connector> T findConnector(String name, List<T> connectors) {
+        for (T c : connectors)
+            if (c.name().equals(name))
+                return c;
         throw new IllegalArgumentException("Unable to find a connector named "+name);
     }
 }
