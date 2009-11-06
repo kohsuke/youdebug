52411d7012fba4abef3e13f49c5b292e2851ba9c
Kohsuke Kawaguchi <kk@kohsuke.org>
2009-11-06 13:57:34 -0800

    more tests

--- src/test/resources/SubStringTest_.groovy
+++ src/test/resources/SubStringTest_.groovy
@@ -3,6 +3,7 @@ import junit.framework.Assert
 vm.breakpoint("SubStringTest",8) {
     println "s="+s;
     Assert.assertEquals(s,"test")
+    Assert.assertEquals(frame(0).s,"test")
     hit++;
 
     vm.exceptionBreakpoint(StringIndexOutOfBoundsException) { e ->
